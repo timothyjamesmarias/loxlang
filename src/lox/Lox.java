c11b729e07076f1d75bbdee81afbcd78688e8244
timothyjamesmarias <timothyjamesmarias@gmail.com>
2024-12-11 20:00:18 -0800

    feat: parser

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -7,6 +7,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import static lox.TokenType.EOF;
+
 public class Lox {
     static boolean hadError = false;
 
@@ -44,10 +46,11 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        if (hadError) return;
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -57,5 +60,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + " ] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
