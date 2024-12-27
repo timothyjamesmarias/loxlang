70e201db6a0fb226f01ef9cd09908cd53360d85a
timothyjamesmarias <timothyjamesmarias@gmail.com>
2024-12-27 15:31:23 -0800

    feat: inheritance

--- src/lox/Expr.java
+++ src/lox/Expr.java
@@ -9,6 +9,7 @@ abstract class Expr {
         R visitCallExpr(Call expr);
         R visitGetExpr(Get expr);
         R visitSetExpr(Set expr);
+        R visitSuperExpr(Super expr);
         R visitGroupingExpr(Grouping expr);
         R visitLiteralExpr(Literal expr);
         R visitLogicalExpr(Logical expr);
@@ -96,6 +97,21 @@ abstract class Expr {
         final Expr object;
         final Token name;
         final Expr value;
+    }
+
+    static class Super extends Expr {
+        Super(Token keyword, Token method) {
+            this.keyword = keyword;
+            this.method = method;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitSuperExpr(this);
+        }
+
+        final Token keyword;
+        final Token method;
     }
 
     static class Grouping extends Expr {
--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -19,6 +19,7 @@ public class GenerateAst {
                 "Call : Expr callee, Token paren, List<Expr> arguments",
                 "Get : Expr object, Token name",
                 "Set : Expr object, Token name, Expr value",
+                "Super : Token keyword, Token method",
                 "Grouping : Expr expression",
                 "Literal : Object value",
                 "Logical : Expr left, Token operator, Expr right",
@@ -29,7 +30,7 @@ public class GenerateAst {
 
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Block : List<Stmt> statements",
-                "Class : Token name, List<Stmt.Function> methods",
+                "Class : Token name, Expr.Variable superclass, List<Stmt.Function> methods",
                 "Expression : Expr expression",
                 "Function : Token name, List<Token> params, List<Stmt> body",
                 "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
