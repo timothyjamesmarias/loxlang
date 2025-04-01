3b55b9812da42268df4b7d26b1329f1c9fea49a2
Timothy Marias <timothyjamesmarias@gmail.com>
2025-03-31 18:59:19 -0700

    hash tables wip

---
+++ clox/table.h
@@ -0,0 +1,28 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "common.h"
+#include "value.h"
+
+
+typedef struct
+{
+    ObjString* key;
+    Value value;
+} Entry;
+
+typedef struct
+{
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table* table);
+
+void freeTable(Table* table);
+
+bool tableSet(Table* table, ObjString* key, Value value);
+
+
+#endif //TABLE_H
